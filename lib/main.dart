e9153461c0973230354d3aea7ef04b82af7efed1
Anton Gushcha <ncrashed@protonmail.com>
2021-09-13 03:02:27 +0300

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ErgveinApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ErgveinApp extends StatelessWidget {
+  const ErgveinApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ergvein',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,15 +22,15 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BalancePage(title: 'Ergvein balance page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class BalancePage extends StatefulWidget {
+  const BalancePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,10 +44,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BalancePage> createState() => _BalancePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _BalancePageState extends State<BalancePage> {
   int _counter = 0;
 
   void _incrementCounter() {
